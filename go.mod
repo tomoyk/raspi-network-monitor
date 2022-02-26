122848b0ee4f62c4f551664537e6c10774829a4b
tomoyk <tmyk.kym@gmail.com>
2022-02-26 06:17:52 +0000

    send icmp packet

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/tomoyk/raspi-network-monitor
 
 go 1.17
 
-require github.com/mattn/go-sqlite3 v1.14.11 // indirect
+require (
+	github.com/mattn/go-sqlite3 v1.14.11 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,60 @@
 package main
 
-import "database/sql"
-import "log"
-import "time"
-import _ "github.com/mattn/go-sqlite3"
+import (
+	"database/sql"
+	"log"
+	"math"
+	"net"
+	"os"
+	"time"
+
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func ping(ipv4_addr string) {
+	c, err := icmp.ListenPacket("udp4", "0.0.0.0")
+	if err != nil {
+		log.Fatalf("ListenPacket: %v", err)
+	}
+	defer c.Close()
+
+	wm := icmp.Message{
+		Type: ipv4.ICMPTypeEcho,
+		Code: 0,
+		Body: &icmp.Echo{
+			ID: os.Getpid() & 0xffff,
+			Seq: 0,
+			Data: []byte("Gopher"),
+		},
+	}
+	wb, err := wm.Marshal(nil)
+	if err != nil {
+		log.Fatalf("Marshal: %v", err)
+	}
+	if _, err := c.WriteTo(wb, &net.UDPAddr{IP: net.ParseIP(ipv4_addr)}); err != nil {
+		log.Fatal(err)
+	}
+
+	rb := make([]byte, 1500)
+	n, _, err := c.ReadFrom(rb)
+	if err != nil {
+		log.Fatalf("ReadFrom: %v", err)
+	}
+	rm, err := icmp.ParseMessage(ipv4.ICMPTypeEcho.Protocol(), rb[:n])
+	if err != nil {
+		log.Fatalf("ICMP Parse: %v", err)
+	}
+	if rm.Type == ipv4.ICMPTypeEchoReply {
+		log.Println("Received ICMP Echo Reply")
+	}
+}
 
 func main() {
 	db, err := sql.Open("sqlite3", "./metrics.db")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("SQL Open: %v", err)
 	}
 	defer db.Close()
 
@@ -18,21 +64,41 @@ func main() {
 	`
 	_, err = db.Exec(sqlStmt)
 	if err != nil {
-		log.Printf("%q: %s\n", err, sqlStmt)
-		return
+		log.Printf("DB Exec: %v %s\n", err, sqlStmt)
 	}
 
-	for i := 0; i < 100; i++ {
-		log.Println(i)
-		stmt, err := db.Prepare("insert into metrics(ts, value) values(?, ?)")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer stmt.Close()
+	dt_start := time.Now()
+	ping("10.204.227.154")
+	dt_end := time.Now()
+	rtt := float64(dt_end.Sub(dt_start))
+	rtt2 := math.Round(rtt*100)/100
+	log.Println("Elapsed:", rtt2)
 
-		dt := time.Now()
-		unix := dt.Unix()
-		stmt.Exec(unix, i%10)
-		time.Sleep(time.Second)
+	stmt, err := db.Prepare("insert into metrics(ts, value) values(?, ?)")
+	if err != nil {
+		log.Fatalf("DB Prepare: %v", err)
 	}
+	defer stmt.Close()
+
+	dt_unix := dt_start.Unix()
+	stmt.Exec(dt_unix, rtt2)
+
+	// 	dt := time.Now()
+	// 	unix := dt.Unix()
+	// 	stmt.Exec(unix, i%10)
+	// for i := 0; i < 100; i++ {
+	// 	log.Println(i)
+	// 	stmt, err := db.Prepare("insert into metrics(ts, value) values(?, ?)")
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	defer stmt.Close()
+
+	// 	dt := time.Now()
+	// 	unix := dt.Unix()
+	// 	stmt.Exec(unix, i%10)
+	// 	time.Sleep(time.Second)
+	// }
+
 }
+
